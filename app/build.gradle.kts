689480155f2049c6e4b9a95e970721885224b75b
Ravi <ravi.sharma02@sap.com>
2026-08-03 13:04:13 +0200

    Release v2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.caesiumstudio.pinstream"
         minSdk = 21
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2"
 
     }
 
