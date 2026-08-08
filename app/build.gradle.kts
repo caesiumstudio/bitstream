c7c42e14f38f21e408823567737645673379dcf9
Ravi <ravi.sharma02@sap.com>
2026-08-08 17:21:49 +0200

    Release v7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.caesiumstudio.bitstream"
         minSdk = 21
         targetSdk = 36
-        versionCode = 6
-        versionName = "6"
+        versionCode = 7
+        versionName = "7"
     }
 
     buildTypes {
